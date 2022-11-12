8f0a107512c3bd07d3c33c1d0c868994eb0ee68a
Rajinth <rajinth3@gmail.com>
2022-11-12 16:08:54 +0530

    Ecommerce Website

---
+++ components/Layout.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Head from "next/head";
+
+import Nav from "./Nav";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Rajin Store </title>
+      </Head>
+      <header>
+        <Nav />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
